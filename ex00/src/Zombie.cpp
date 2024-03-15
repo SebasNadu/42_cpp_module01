c1bdc2a947b7bbfc68ddea4aa978f72a07d58192
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2024-03-15 11:21:12 +0100

    Fix: Add files for newZombie and randomChump functions

--- ex00/src/Zombie.cpp
+++ ex00/src/Zombie.cpp
@@ -11,13 +11,3 @@ Zombie::~Zombie() {
 void Zombie::announce(void) const {
   std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
-
-Zombie *newZombie(std::string name) {
-  Zombie *zombie = new Zombie(name);
-  return zombie;
-}
-
-void randomChump(std::string name) {
-  Zombie zombie(name);
-  zombie.announce();
-}
