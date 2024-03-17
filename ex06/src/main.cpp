77a6a90d7f0ad3a52fe51b2e24383e6700dd02d4
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2024-03-17 13:51:42 +0100

    Fix: ex06 err message for wrong input

--- ex06/src/main.cpp
+++ ex06/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sebasnadu <johnavar@student.42berlin.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 01:07:53 by sebasnadu         #+#    #+#             */
-/*   Updated: 2024/03/17 13:22:51 by sebasnadu        ###   ########.fr       */
+/*   Updated: 2024/03/17 13:50:44 by sebasnadu        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int main(int ac, char **av) {
   Harl harl;
 
   if (ac != 2) {
-    std::cout << "Error: Too many arguments:\n\tUsage: ./harl "
+    std::cout << "Error: Wrong number of arguments:\n\tUsage: ./harl "
                  "[DEBUG|INFO|WARNING|ERROR]"
               << std::endl;
     return 1;
